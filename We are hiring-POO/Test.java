aac39a91e5fdbf608f0cdc21125bb44ad84f8ce6
Elvis <marianelvis.popa@yahoo.com>
2022-03-14 15:14:33 +0200

    first commit

---
+++ We are hiring-POO/Test.java
@@ -0,0 +1,9 @@
+//public class Test {
+//    public static void main(String[] args) {
+//        Application application =Application.getInstance();
+//        Company Google = new Company("Google", null, null, null);
+//        Company Amazon = new Company("Amazon", null, null, null);
+//        System.out.println(Google);
+//        System.out.println(Amazon);
+//    }
+//}
